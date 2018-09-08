2a9a8997ecccd6da4a4809c109c4d510e713fa9f
Anupam Dagar <dagaranupam@gmail.com>
2018-09-09 00:01:00 +0530

    Add assignment 1.

---
+++ assignment 1/assignment1.pl
@@ -0,0 +1,69 @@
+% English sentence
+
+sentence --> noun_phrase, predicate.
+noun_phrase --> article, noun.
+predicate --> verb.
+article --> [a].
+article --> [the].
+noun --> [cat].
+noun --> [dog].
+verb --> [runs].
+verb --> [walks].
+
+% Palindrome check
+gram --> [].
+
+gram --> [a].
+gram --> [b].
+gram --> [c].
+gram --> [d].
+gram --> [e].
+gram --> [f].
+gram --> [g].
+gram --> [h].
+gram --> [i].
+gram --> [j].
+gram --> [k].
+gram --> [l].
+gram --> [m].
+gram --> [n].
+gram --> [o].
+gram --> [p].
+gram --> [q].
+gram --> [r].
+gram --> [s].
+gram --> [t].
+gram --> [u].
+gram --> [v].
+gram --> [w].
+gram --> [x].
+gram --> [y].
+gram --> [z].
+
+gram --> [a],gram,[a].
+gram --> [b],gram,[b].
+gram --> [c],gram,[c].
+gram --> [d],gram,[d].
+gram --> [e],gram,[e].
+gram --> [f],gram,[f].
+gram --> [g],gram,[g].
+gram --> [h],gram,[h].
+gram --> [i],gram,[i].
+gram --> [j],gram,[j].
+gram --> [k],gram,[k].
+gram --> [l],gram,[l].
+gram --> [m],gram,[m].
+gram --> [n],gram,[n].
+gram --> [o],gram,[o].
+gram --> [p],gram,[p].
+gram --> [q],gram,[q].
+gram --> [r],gram,[r].
+gram --> [s],gram,[s].
+gram --> [t],gram,[t].
+gram --> [u],gram,[u].
+gram --> [v],gram,[v].
+gram --> [w],gram,[w].
+gram --> [x],gram,[x].
+gram --> [y],gram,[y].
+gram --> [z],gram,[z].
+
